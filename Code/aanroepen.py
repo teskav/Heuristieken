15ae3a7b3c24f03800f45800cbcc8ace12f0a42c
Wies de Wit <Wies@wcw-stud-145-109-17-156.wireless.uva.nl>
2019-05-07 18:20:29 +0200

    aanroepen nieuwe update

---
+++ Code/aanroepen.py
@@ -0,0 +1,132 @@
+from parcel import Parcel
+from spacecraft import Spacecraft
+from spacefreight import SpaceFreight
+from random_algorithms_new import *
+from first_fit_algorithms import *
+from iterative_algorithms_new import *
+from hill_climber import *
+import pandas as pd
+
+spacefreight = SpaceFreight()
+
+def aanroepen_random():
+    iterations_dataframe = pd.DataFrame()
+    max_iterations = 100
+    count = 0
+    best_solution = random_greedy()
+    dataframe_row = spacefreight.save_iteration(best_solution, count)
+    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+    while count < max_iterations:
+        count += 1
+        solution = random_greedy()
+
+        # save to dataframe
+        dataframe_row = spacefreight.save_iteration(solution, count)
+        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+        # add number of packed parcels of solution to list to plot
+        # plot_parcels.append(len(spacefreight.all_parcels)-solution.not_bring)
+
+        # check if costs better
+        if solution.not_bring < best_solution.not_bring:
+            best_solution = solution
+        elif solution.not_bring == best_solution.not_bring and solution.costs < best_solution.costs:
+            best_solution = solution
+
+    print("Iterations:", count)
+    spacefreight.printing(best_solution)
+
+    return iterations_dataframe
+
+def aanroepen_constrained():
+    iterations_dataframe = pd.DataFrame()
+    max_iterations = 100
+    count = 0
+    best_solution = random_constraints()
+    dataframe_row = spacefreight.save_iteration(best_solution, count)
+    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+    while count < max_iterations:
+        count += 1
+        solution = random_constraints()
+
+        # save to dataframe
+        dataframe_row = spacefreight.save_iteration(solution, count)
+        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+        # add number of packed parcels of solution to list to plot
+        # plot_parcels.append(len(spacefreight.all_parcels)-solution.not_bring)
+
+        # check if costs better
+        if solution.not_bring < best_solution.not_bring:
+            best_solution = solution
+        elif solution.not_bring == best_solution.not_bring and solution.costs < best_solution.costs:
+            best_solution = solution
+
+    print("Iterations:", count)
+    spacefreight.printing(best_solution)
+
+    return iterations_dataframe
+
+def aanroepen_random_all():
+    iterations_dataframe = pd.DataFrame()
+    max_iterations = 100
+    # run all parcels random
+    best_solution = random_all_parcels()
+    count = 0
+    dataframe_row = spacefreight.save_iteration(best_solution, count)
+    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+    while count < max_iterations:
+        count += 1
+        solution = random_all_parcels()
+
+        # save to dataframe
+        dataframe_row = spacefreight.save_iteration(solution, count)
+        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+        # check if costs better
+        if solution.costs < best_solution.costs:
+            best_solution = solution
+
+    print("Iterations:", count)
+    spacefreight.printing(best_solution)
+
+    return iterations_dataframe
+
+def aanroepen_constrained_all():
+    iterations_dataframe = pd.DataFrame()
+    max_iterations = 100
+    # run all parcels constrained
+    best_solution = random_constraints_all()
+    count = 0
+    dataframe_row = spacefreight.save_iteration(best_solution, count)
+    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+    while count < max_iterations:
+        count += 1
+        solution = random_constraints_all()
+
+        # save to dataframe
+        dataframe_row = spacefreight.save_iteration(solution, count)
+        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
+
+        # check if costs better
+        if solution.costs < best_solution.costs:
+            best_solution = solution
+
+    print("Iterations:", count)
+    spacefreight.printing(best_solution)
+
+    return iterations_dataframe
+
+def aanroepen_hill_climber():
+    iterations_dataframe = pd.DataFrame()
+    max_iterations = 100
+    # HILL CLIMBER
+    iterations_dataframe, count, current_solution = hill_climber(iterations_dataframe, max_iterations)
+    print("Iterations:", count)
+    spacefreight.printing(current_solution)
+
+    return iterations_dataframe
--- main.py
+++ main.py
@@ -16,120 +16,30 @@ from iterative_algorithms_new import *
 from hill_climber import *
 import pandas as pd
 import matplotlib.pyplot as plt
+from aanroepen import *
 
 spacefreight = SpaceFreight()
 
 text = input("Please give input: ")
-
-# DATAFRAME & ITERATIONS (ALTIJD AAN)
-iterations_dataframe = pd.DataFrame()
-max_iterations = 100
 
 # histogram plotten
 # plot_parcels = []
 
 # RUN NOT ALL PARCELS
 if text == 'random':
-    count = 0
-    best_solution = random_greedy()
-    dataframe_row = spacefreight.save_iteration(best_solution, count)
-    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-    while count < max_iterations:
-        count += 1
-        solution = random_greedy()
-
-        # save to dataframe
-        dataframe_row = spacefreight.save_iteration(solution, count)
-        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-        # add number of packed parcels of solution to list to plot
-        # plot_parcels.append(len(spacefreight.all_parcels)-solution.not_bring)
-
-        # check if costs better
-        if solution.not_bring < best_solution.not_bring:
-            best_solution = solution
-        elif solution.not_bring == best_solution.not_bring and solution.costs < best_solution.costs:
-            best_solution = solution
-
-    print("Iterations:", count)
-    spacefreight.printing(best_solution)
+    iterations_dataframe = aanroepen_random()
 
 if text == 'constrained':
-    count = 0
-    best_solution = random_constraints()
-    dataframe_row = spacefreight.save_iteration(best_solution, count)
-    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-    while count < max_iterations:
-        count += 1
-        solution = random_constraints()
-
-        # save to dataframe
-        dataframe_row = spacefreight.save_iteration(solution, count)
-        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-        # add number of packed parcels of solution to list to plot
-        # plot_parcels.append(len(spacefreight.all_parcels)-solution.not_bring)
-
-        # check if costs better
-        if solution.not_bring < best_solution.not_bring:
-            best_solution = solution
-        elif solution.not_bring == best_solution.not_bring and solution.costs < best_solution.costs:
-            best_solution = solution
-
-    print("Iterations:", count)
-    spacefreight.printing(best_solution)
+    iterations_dataframe = aanroepen_constrained()
 
 if text == 'random all':
-    # run all parcels random
-    best_solution = random_all_parcels()
-    count = 0
-    dataframe_row = spacefreight.save_iteration(best_solution, count)
-    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-    while count < max_iterations:
-        count += 1
-        solution = random_all_parcels()
-
-        # save to dataframe
-        dataframe_row = spacefreight.save_iteration(solution, count)
-        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-        # check if costs better
-        if solution.costs < best_solution.costs:
-            best_solution = solution
-
-    print("Iterations:", count)
-    spacefreight.printing(best_solution)
+    iterations_dataframe = aanroepen_random_all()
 
 if text == 'constrained all':
-    # run all parcels constrained
-    best_solution = random_constraints_all()
-    count = 0
-    dataframe_row = spacefreight.save_iteration(best_solution, count)
-    iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-    while count < max_iterations:
-        count += 1
-        solution = random_constraints_all()
-
-        # save to dataframe
-        dataframe_row = spacefreight.save_iteration(solution, count)
-        iterations_dataframe = iterations_dataframe.append(dataframe_row, ignore_index=True)
-
-        # check if costs better
-        if solution.costs < best_solution.costs:
-            best_solution = solution
-
-    print("Iterations:", count)
-    spacefreight.printing(best_solution)
+    iterations_dataframe = aanroepen_constrained_all()
 
 if text == 'hill climber':
-    # HILL CLIMBER
-    iterations_dataframe, count, current_solution = hill_climber(iterations_dataframe, max_iterations)
-    print("Iterations:", count)
-    spacefreight.printing(current_solution)
+    iterations_dataframe = aanroepen_hill_climber()
 
 # iterations_dataframe.to_csv(r'../Heuristieken/Outputs/Output5.csv')
 
