d73fe7f7bad18ed92505b31f541c231750677bd1
Sofie Löhr <sofielohr@wcw-stud-145-109-46-192.wireless.uva.nl>
2019-05-21 10:43:00 +0200

    calculations added

---
+++ Code/calculations.py
@@ -0,0 +1,14 @@
+# HEURISTIEKEN
+# April - Mei 2019
+# Space Freight
+# Sofie Löhr, Teska Vaessen & Wies de Wit
+"""
+Script to calculate .... NOG AANPASSEN
+"""
+import pandas as pd
+
+
+output_location = '../Outputs/Random/test.csv'
+data = pd.read_csv(output_location, index_col=0)
+
+print(data.head())
