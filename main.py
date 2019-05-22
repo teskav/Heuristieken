984cbf4e333b1dc7fd3987343bddf98e810abba8
Wies de Wit <Wies@wcw-stud-145-109-3-172.wireless.uva.nl>
2019-05-22 12:04:27 +0200

    ouput HC spacecrafts CL1

--- main.py
+++ main.py
@@ -75,12 +75,12 @@ if algorithm == 'political constraints' and list != '3':
 # dataframe.to_csv(r'../Heuristieken/Outputs/Random/CL2_100000.csv')
 
 # HILL CLIMBER (SPECIFY PARCEL OR SPACECRAFT SWITCH)
-# iterations_dataframe.to_csv(r'../Heuristieken/Outputs/Hill_Climber/iterations_spacecrafts_CL2_30x2000.csv')
-# runs_dataframe.to_csv(r'../Heuristieken/Outputs/Hill_Climber/runs_spacecrafts_CL2_30x2000.csv')
+iterations_dataframe.to_csv(r'../Heuristieken/Outputs/Hill_Climber/iterations_spacecrafts_CL1_30x2000.csv')
+runs_dataframe.to_csv(r'../Heuristieken/Outputs/Hill_Climber/runs_spacecrafts_CL1_30x2000.csv')
 
 # SIMULATED ANNEALING
-iterations_dataframe.to_csv(r'../Heuristieken/Outputs/Simulated_Annealing/iterations_SA_CL1_lin_30x2000.csv')
-runs_dataframe.to_csv(r'../Heuristieken/Outputs/Simulated_Annealing/runs_SA_CL1_lin_30x2000.csv')
+# iterations_dataframe.to_csv(r'../Heuristieken/Outputs/Simulated_Annealing/iterations_SA_CL1_lin_30x2000.csv')
+# runs_dataframe.to_csv(r'../Heuristieken/Outputs/Simulated_Annealing/runs_SA_CL1_lin_30x2000.csv')
 
 # POLITICAL CONSTRAINTS
 # dataframe.to_csv(r'../Heuristieken/Outputs/Political_Constraints/test.csv')
